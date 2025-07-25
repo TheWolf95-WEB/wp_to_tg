8ea7a96abaf50dc26b45549f97b87712e53b0346
ND_Web <55359442+TheWolf95-WEB@users.noreply.github.com>
2025-07-25 20:44:03 +0300

    Update config.py

--- wp_bot/config.py
+++ wp_bot/config.py
@@ -5,4 +5,8 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DB_PATH = os.getenv("DB_PATH", "telegram_wp_users.db")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(","))) if os.getenv("ADMIN_IDS") else []
+SERVICE_NAME = os.getenv("SERVICE_NAME", "wp_bot.service")
+GITHUB_SECRET = os.getenv("GITHUB_SECRET", "")
+
+# 💡 Преобразуем строку в список чисел
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
