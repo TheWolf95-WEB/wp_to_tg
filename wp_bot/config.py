0d32acb49e740a7a9c283c44738c08ed41a2c45f
ND_Web <55359442+TheWolf95-WEB@users.noreply.github.com>
2025-07-25 20:35:39 +0300

    Update config.py

--- wp_bot/config.py
+++ wp_bot/config.py
@@ -5,3 +5,4 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DB_PATH = os.getenv("DB_PATH", "telegram_wp_users.db")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(","))) if os.getenv("ADMIN_IDS") else []
